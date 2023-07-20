4f3056e2d04f2b6df027d820c6c279b4272c0005
Igor Trimailov <itrimailov@intermedia.com>
2023-07-20 14:53:21 +0100

    auth form

---
+++ lib/api.ts
@@ -0,0 +1,49 @@
+type FetcherParams<T> = {
+    url: string;
+    method: "GET" | "POST" | "PUT" | "DELETE";
+    body?: T;
+    json?: boolean;
+};
+
+const fetcher = async <T>({
+    url,
+    method,
+    body,
+    json = true,
+}: FetcherParams<T>) => {
+    const res = await fetch(url, {
+        method,
+        ...(body && { body: JSON.stringify(body) }),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error("API error");
+    }
+
+    if (json) {
+        const data = await res.json();
+        return data.data;
+    }
+};
+
+export const register = (user: AuthFormFields) => {
+    console.log("register api");
+    return fetcher<AuthFormFields>({
+        url: "/api/register",
+        method: "POST",
+        body: user,
+    });
+};
+
+export const signin = (user: AuthFormFields) => {
+    console.log("signin api");
+    return fetcher<AuthFormFields>({
+        url: "/api/signin",
+        method: "POST",
+        body: user,
+    });
+};
